ca1f9d742a164fcca3ec5c0020e35311d0b04bff
王庭茂 <wtmtim@126.com>
2014-02-15 15:35:11 +0800

    看帖（不包括楼中楼）已完成！
    Signed-off-by: 王庭茂 <wtmtim@126.com>

--- CSharp/thread.cs
+++ CSharp/thread.cs
@@ -78,9 +78,161 @@ namespace Opentieba
             maxPage = th["page"]["total_page"].Value<long>();
             kw = new bar(th["forum"]["name"].Value<String>());
         }
+        public List<basePost> listpost(long page)
+        {
+            JObject jo = JSON.parse(_stbapi.sendTieba("/c/f/pb/page", "kz=" + tid + "&pn=" + page, ""));
+            JEnumerable<JToken> posts = jo["post_list"].Children();
+            List<basePost> lbp = new List<basePost>();
+            foreach (JToken pt in posts)
+            {
+                List<postContent> pc = new List<postContent>();
+                JEnumerable<JToken> jecon = pt["content"].Children();
+                foreach (JToken conjt in jecon)
+                {
+                    pc.Add(postContent.byJtoken(conjt));
+                }
+                lbp.Add(new basePost(new userInBar(pt["author"]["id"].Value<long>(), pt["author"]["name"].Value<String>(),
+                    pt["author"]["is_like"].Value<bool>(), pt["author"]["level_id"].Value<int>(), pt["author"]["portrait"].Value<String>())
+                    , pc.ToArray(), pt["floor"].Value<long>(), pt["id"].Value<long>(), pt["sub_post_number"].Value<long>(),
+                    pt["time"].Value<long>(), pt["title"].Value<String>()));
+            }
+            return lbp;
+        }
+    }
+    public class postContent
+    {
+        /// <summary>
+        /// 引用JS里的一段代码：
+        /// <code>
+        ///                         function showcon(con,contarr){
+        ///                            for (var i = 0; i &lt;; contarr.length; i++) {
+        ///                                (function (a) {
+        ///                                    if (a.type == 0) {
+        ///                                        var pp = $("&lt;p&gt;正在加载.....&lt;/p&gt;");
+        ///                                       _.js.encodeHtml(a.text, function (q) {
+        ///                                            pp.html(q);
+        ///                                        });
+        ///                                        con.append(pp);
+        ///                                    } else if (a.type == 1) {
+        ///                                        con.append($("&lt;a style='color: #9c3f38;'&gt;" + enchtml(a.text) + "&lt;/a&gt;").click(function () {
+        ///                                            //todo something open link
+        ///                                            if (a.link.match(/^http:\/\/tieba.baidu.com\/p\/\d+$/) != null) {
+        ///                                                _.contureDo("是否打开帖子" + enchtml(a.link.substr(25)) + "？", function () {
+        ///                                                    seeThread(a.link.substr(25), kw, fid);
+        ///                                                });
+        ///                                            } else if (a.link.match(/^\//)) {
+        ///                                                var win = window.open("http://tieba.baidu.com" + a.link, "贴吧链接");
+        ///                                                win.setInterval(function () {
+        ///                                                    win.document.title = "贴吧链接"
+        ///                                                }, 500);
+        ///                                            } else {
+        ///                                                var win = window.open(a.link, "贴吧链接");
+        ///                                                win.setInterval(function () {
+        ///                                                    win.document.title = "贴吧链接"
+        ///                                                }, 500);
+        ///                                            }
+        ///                                        })).append($("&lt;br&gt;"));
+        ///                                   } else if (a.type == 2) {
+        ///                                       con.append($(faceele(a.text)).click(function () {
+        ///                                           console.info(a);
+        ///                                       }));
+        ///                                   } else if (a.type == 3) {
+        ///                                       var sizearr = a.bsize.split(",", 2);
+        ///                                       var img = $("&lt;img src='' width='" + sizearr[0] + "' height='" + sizearr[1] + "'&gt;");
+        ///                                       con.append(img).append($("&lt;br&gt;"));
+        ///                                       outdomainpic(img[0], a.src);
+        ///                                       var picloading = $("&lt;div style='margin: 15px; background-color: rgba(0, 0, 0, 0.26);'&gt;此处的图像正在加载&lt;/div&gt;");
+        ///                                       img.before(picloading);
+        ///                                       img.load(function () {
+        ///                                           picloading.animate({height: "0px", opacity: 0}, 300);
+        ///                                       });
+        ///                                       img.mouseenter(function () {
+        ///                                           img.stop(false, false, false);
+        ///                                           img.transit({opacity: 0.9,scale:1.6}, 250);
+        ///                                       }).mouseleave(function () {
+        ///                                               img.stop(false, false, false);
+        ///                                               img.transit({opacity: 1,scale:1}, 250);
+        ///                                           });
+        ///                                       setTimeout(function () {
+        ///                                           picloading.animate({height: "0px", opacity: 0}, 300);
+        ///                                       }, 5000);
+        ///                                   } else if (a.type == 4) {
+        ///                                       con.append($("&lt;a style='color: #689269;'&gt;" + enchtml(a.text) + "&lt;/a&gt;").click(function () {
+        ///                                           // todo...
+        ///                                       })).append($("&lt;br&gt;"));
+        ///                                       _.ReUpadtaEffect();
+        ///                                   } else if (a.type == 5) {
+        ///                                       con.append($("&lt;iframe sandbox='allow-same-origin allow-forms allow-scripts' style='width: 100%; height: 600px;' src='" + a.text + "'&gt;&lt;/iframe&gt;"));
+        ///                                   } else {
+        ///                                       con.append($("&lt;div style='color: #1e6c37;'&gt;[未知类型：" + a.type + "]“" + enchtml(JSON.stringify(a)) + "”&lt;/div&gt;")).append("&lt;br&gt;");
+        ///                                   }
+        ///                               })(contarr[i]);
+        ///                           }
+        ///                       }
+        /// </code>
+        /// </summary>
+        public readonly int type;
+        public readonly String text;
+        public readonly String link = "";
+        public readonly String c = "";
+        public readonly int[] bsize = { 0, 0 };
+        public readonly String src = "";
+        public readonly long uid = 0;
+        public postContent(int type, String text, String link, String c, int[] bsize, String src, long uid)
+        {
+            this.type = type;
+            this.text = text;
+            this.link = link;
+            this.c = c;
+            this.bsize = bsize;
+            this.src = src;
+            this.uid = uid;
+        }
+        public static postContent byJtoken(JToken jy)
+        {
+            int type;
+            String text;
+            String link = "";
+            String c = "";
+            int[] bsize = { 0, 0 };
+            String src = "";
+            long uid = 0;
+            type = jy["type"].Value<int>();
+            text = jy["text"].Value<String>();
+            if (jy["link"] != null)
+            {
+                link = jy["link"].Value<String>();
+            }
+            if (jy["c"] != null)
+            {
+                c = jy["c"].Value<String>();
+            }
+            if (jy["bsize"] != null)
+            {
+                bsize = (int[])from String p in jy["bsize"].Value<String>().Split(",".ToCharArray())
+                               select Int32.Parse(p);
+            }
+            return new postContent(type, text, link, c, bsize, src, uid);
+        }
     }
     public class basePost
     {
-
+        public readonly userInBar author;
+        public readonly postContent[] content;
+        public readonly long floor;
+        public readonly long id;
+        public readonly long sub_post_number;
+        public readonly long time;
+        public readonly String title;
+        public basePost(userInBar author, postContent[] content, long floor, long id, long supn, long time, String title)
+        {
+            this.author = author;
+            this.content = content;
+            this.floor = floor;
+            this.id = id;
+            this.sub_post_number = supn;
+            this.time = time;
+            this.title = title;
+        }
     }
 }
