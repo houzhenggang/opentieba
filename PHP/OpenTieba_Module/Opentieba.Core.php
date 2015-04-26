1ddf6d0ab4459ce2c7a364e4845817208b0775ce
Sunbread <xh3061895@gmail.com>
2015-04-26 20:51:06 +0800

    BDUSS创建

--- PHP/OpenTieba_Module/Opentieba.Core.php
+++ PHP/OpenTieba_Module/Opentieba.Core.php
@@ -3,6 +3,7 @@ class Core // 谨慎改动
 {
     // require("Opentieba.Core.Config.php");
     require("Opentieba.Core.SendTieba.php");
+	require("Opentieba.Core.BDUSS.php");
 	// 欢迎各位有识之士编写内核代码，并把引用加入到该文件中。
 }
 ?>
